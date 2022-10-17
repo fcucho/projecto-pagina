848e52d1c2181f53975d8cae05440ef150f4bb73
fernando <fernandocuccioletti@gmail.com>
2022-10-17 11:51:01 -0300

    cambios organizacion carpeta

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+import NavBar from './components/profile/navbar';
+
+function Home() {
+
+
+  
+    return (
+      <div>
+        
+        <h1 class="visible">Hola mundo</h1>
+        
+      </div>
+    );
+  }
+  
+  export default Home;
+  
